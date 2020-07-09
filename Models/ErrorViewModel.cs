c85b0739551942c89e5fe536c5caf0e10b5baa50
kailane <kailaneps@hotmail.com>
2020-07-09 17:33:44 -0300

    testeAspNet

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _35_AspNet_Core__Razor.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
